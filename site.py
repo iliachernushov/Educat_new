4e91e4824b875e44713129eb307b6c824e262ffa
iliachernushov <ilia.chernushov@gmail.com>
2017-07-15 21:02:02 +0300

    Правки шаблона и корневого файла
    Теперь на странице выводится город. В исходном файле добавил заголовку
    под обращение к БД

--- site.py
+++ site.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template
 
+try:
+	from db import get_programs_list
+except:
+	pass
 
 app = Flask(__name__)
 
@@ -7,7 +11,10 @@ app = Flask(__name__)
 @app.route('/index.html')
 def index():
 
-    edu_programs_list = [{'name':'Первая обучающая программа', 'description':'Описание программы 1. Много букв...', 'city':'Москва', 'link':'https://www.rambler.ru/'}, {'name':'Вторая обучающая программа', 'description':'Описание программы 2. Много букв...', 'city':'Тамбов', 'link':'https://www.yandex.ru/'}]
+    try:
+    	edu_programs_list = get_programs_list(30)
+    except:
+    	edu_programs_list = [{'name':'Первая обучающая программа', 'description':'Описание программы 1. Много букв...', 'city':'Москва', 'link':'https://www.rambler.ru/'}, {'name':'Вторая обучающая программа', 'description':'Описание программы 2. Много букв...', 'city':'Тамбов', 'link':'https://www.yandex.ru/'}]
     
     return render_template('index.html', edu_programs_list=edu_programs_list)
 
